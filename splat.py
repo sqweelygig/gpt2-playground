7d03dea2135baa91d2d9f0e3f498f723056927cd
Andrew Lucas <sqweelygig@googlemail.com>
2020-02-18 09:44:46 +0000

    Added basic text to speech

--- splat.py
+++ splat.py
@@ -1,9 +1,12 @@
 import gpt_2_simple as gpt2
 import os
 import requests
+import pyttsx3 as voice
+import time
 
-tuning_name = "shakespeare"
+language = "en"
 model_name = "medium"
+tuning_name = "shakespeare"
 starter_text = "Last Friday I"
 
 models_available = {
@@ -19,19 +22,31 @@ tunings_available = {
 
 model_dir = models_available[model_name]
 if not os.path.isdir(os.path.join("models", model_dir)):
-    print(f"Downloading {model_name} model... ({model_dir})")
+    print(f"Downloading {model_name} model...")
     gpt2.download_gpt2(model_name=model_dir)
 
 tuning_file = os.path.join("tunings", f"{tuning_name}.txt")
 if not os.path.isfile(tuning_file):
+    print(f"Downloading {tuning_name} library...")
     tuning_url = tunings_available[tuning_name]
     data = requests.get(tuning_url)
     with open(tuning_file, 'w') as f:
         f.write(data.text)
 
+voice = voice.init()
+
 while True:
     sess = gpt2.start_tf_sess()
     run_name = f"{tuning_name}-{model_name}"
+    print(f"Tuning {run_name}...")
     gpt2.finetune(sess, tuning_file, model_name=model_dir, steps=1, overwrite=True, run_name=run_name)
-    gpt2.generate(sess, prefix=starter_text, length=31, run_name=run_name)
+    print(f"Generating text using {run_name}...")
+    generated_text = gpt2.generate(sess, prefix=starter_text, length=128, run_name=run_name, return_as_list=True)[0]
+    print(generated_text)
+    print(f"Speaking text...")
+    sentences = generated_text.split(".")[:-1]
+    for sentence in sentences:
+        voice.say(sentence)
+        voice.runAndWait()
+        time.sleep(0.15)
     gpt2.reset_session(sess)
