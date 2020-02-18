1c63c5c548ff69f6b225e709aef308b49a4635fb
Andrew Lucas <sqweelygig@googlemail.com>
2020-02-18 10:36:29 +0000

    Added frankenstein, improved speech

--- splat.py
+++ splat.py
@@ -1,12 +1,11 @@
 import gpt_2_simple as gpt2
 import os
 import requests
-import pyttsx3 as voice
-import time
+import pyttsx3 as text_to_speech
 
 language = "en"
 model_name = "medium"
-tuning_name = "shakespeare"
+tuning_name = "frankenstein"
 starter_text = "Last Friday I"
 
 models_available = {
@@ -18,6 +17,7 @@ models_available = {
 tunings_available = {
     "shakespeare": "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt",
     "fortune-cookies": "https://raw.githubusercontent.com/reggi/fortune-cookie/master/fortune-cookies.txt",
+    "frankenstein": "https://www.gutenberg.org/files/84/84-0.txt"
 }
 
 model_dir = models_available[model_name]
@@ -33,7 +33,14 @@ if not os.path.isfile(tuning_file):
     with open(tuning_file, 'w') as f:
         f.write(data.text)
 
-voice = voice.init()
+
+def say_sentences(text):
+    voice = text_to_speech.init()
+    complete_sentences = text.split(".")[:-1]
+    text_to_speak = ". ".join(complete_sentences)
+    voice.say(text_to_speak)
+    voice.runAndWait()
+
 
 while True:
     sess = gpt2.start_tf_sess()
@@ -44,9 +51,5 @@ while True:
     generated_text = gpt2.generate(sess, prefix=starter_text, length=128, run_name=run_name, return_as_list=True)[0]
     print(generated_text)
     print(f"Speaking text...")
-    sentences = generated_text.split(".")[:-1]
-    for sentence in sentences:
-        voice.say(sentence)
-        voice.runAndWait()
-        time.sleep(0.15)
+    say_sentences(generated_text)
     gpt2.reset_session(sess)
