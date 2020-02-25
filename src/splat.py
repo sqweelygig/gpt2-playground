86ebccec9f9b2c7a5efdc08ad2e2c33632f52fc6
Andrew Lucas <sqweelygig@googlemail.com>
2020-02-25 17:56:29 +0000

    This commit went long, sorry
    * Put read-only class properties into @property functions
    * Moved logging into each function
    * Smart load checkpoint or model data

--- src/splat.py
+++ src/splat.py
@@ -22,25 +22,44 @@ class UnicornAI:
     }
 
     def __init__(self, model="small", tuning="frankenstein", log_function=lambda x: print(x)):
-        self.run_name = f"{tuning}-{model}"
-        self.model_name = UnicornAI.models_available[model]
-        self.tuning_path = os.path.join("tunings", f"{tuning}.txt")
-        if not os.path.isdir(os.path.join("models", self.model_name)):
-            log_function(f"Downloading {model} model...")
-            gpt2.download_gpt2(model_name=self.model_name)
-        if not os.path.isfile(self.tuning_path):
-            log_function(f"Downloading {tuning} library...")
-            tuning_url = UnicornAI.tunings_available[tuning]
-            tuning_text = requests.get(tuning_url).text
-            with open(self.tuning_path, "w") as file:
-                file.write(tuning_text)
-        log_function("Starting brain...")
-        self.fine_tune()
+        self.tuning_description = tuning
+        self.model_description = model
+        self.log_function = log_function
+        self.download_model()
+        self.download_tuning()
         log_function("Starting voice...")
         pygame.mixer.init()
 
-    @staticmethod
-    def say_sentences(text_to_speak="Hello world!"):
+    @property
+    def model_name(self):
+        return UnicornAI.models_available[self.model_description]
+
+    @property
+    def tuning_path(self):
+        return os.path.join("tunings", f"{self.tuning_description}.txt")
+
+    @property
+    def run_name(self):
+        return f"{self.tuning_description}-{self.model_description}"
+
+    def download_model(self):
+        if not os.path.isdir(os.path.join("models", self.model_name)):
+            self.log_function(f"* Downloading {self.model_description} model...")
+            gpt2.download_gpt2(model_name=self.model_name)
+
+    def download_tuning(self):
+        if not os.path.isfile(self.tuning_path):
+            self.log_function(f"* Downloading {self.tuning_description} library...")
+            tuning_url = UnicornAI.tunings_available[self.tuning_description]
+            tuning_text = requests.get(tuning_url).text
+            with open(self.tuning_path, "w") as file:
+                file.write(tuning_text)
+
+    def say_sentences(self, text_to_speak="Hello world!"):
+        self.log_function("* Speaking...")
+        self.log_function("\"\"\"")
+        self.log_function(text_to_speak)
+        self.log_function("\"\"\"")
         speech = TextToSpeech(text=text_to_speak, lang=UnicornAI.language)
         seconds = int(time.time())
         filename = f"tmp/{seconds}.mp3"
@@ -49,26 +68,31 @@ class UnicornAI:
         pygame.mixer.music.play()
 
     def generate(self, prefix="Last Friday I", length=50):
+        self.log_function("* Generating text...")
+        self.log_function(f"  * {self.model_description} language model tuned towards {self.tuning_description}.")
         brain = gpt2.start_tf_sess()
-        gpt2.load_gpt2(brain, run_name=self.run_name)
-        text = gpt2.generate(brain, prefix=prefix, length=length, run_name=self.run_name, return_as_list=True)[0]
+        run_name = self.run_name if os.path.isdir(os.path.join("checkpoint", self.run_name)) else None
+        model_name = None if os.path.isdir(os.path.join("checkpoint", self.run_name)) else self.model_name
+        gpt2.load_gpt2(brain, run_name=run_name, model_name=model_name)
+        text = gpt2.generate(
+            brain,
+            prefix=prefix, length=length, run_name=run_name, return_as_list=True, model_name=self.model_name,
+        )[0]
         gpt2.reset_session(brain)
         return text
 
-    def fine_tune(self):
+    def fine_tune(self, steps=1):
+        self.log_function(f"* Fine tuning towards {self.tuning_description}...")
         brain = gpt2.start_tf_sess()
         gpt2.finetune(
             brain, self.tuning_path,
-            model_name=self.model_name, steps=1, overwrite=True, run_name=self.run_name
+            model_name=self.model_name, steps=steps, run_name=self.run_name
         )
         gpt2.reset_session(brain)
 
 
 if __name__ == "__main__":
     unicorn = UnicornAI()
-    print(f"Generating text using {unicorn.run_name}...")
     generated_text = unicorn.generate()
-    print("Speaking text...")
-    print(generated_text)
     unicorn.say_sentences(generated_text)
-    time.sleep(60)
+    unicorn.fine_tune()
